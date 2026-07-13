72be353a06c6dd51acbd89215eb9f712e2d44e5d
sharishkhan <sharishkhan07@gmail.com>
2026-07-13 23:08:53 +0530

    fix: allow Vercel frontend origin

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -16,11 +16,14 @@ class Config:
     GEMINI_MODEL = os.getenv("GEMINI_MODEL", "gemini-2.5-flash")
     SUMMARY_CHUNK_CHARACTER_LIMIT = 24000
 
-    CORS_ORIGINS = [
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
-        "http://127.0.0.1:5174",
-        "http://localhost:5174",
+   CORS_ORIGINS = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://127.0.0.1:5174",
+    "http://localhost:5174",
+
+    "https://docmind-ai-three.vercel.app",
+    "https://docmind-ai-git-main-sharish-k.vercel.app",
     ]
 
     @classmethod
